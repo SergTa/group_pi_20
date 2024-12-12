dcdbdb9fa73e8333b55c4b684f7d38909b102004
krassilnikov <153736692+krassilnikov@users.noreply.github.com>
2024-12-13 02:12:57 +0300

    Добавил Flask + Vue
    Добавил Flask + Vue

--- src/models/predict_model_wrapper.py
+++ src/models/predict_model_wrapper.py
@@ -1,6 +1,7 @@
 import pickle
+import os
 
-def predict_release_risk(release_properties, path_to_model = 'trained/model_lr.pkl'):
+def predict_release_risk(release_properties, path_to_model='trained/model_lr.pkl'):
     """
     Пример того что нужно передавать в `release_properties`
     release_properties = [[
@@ -12,7 +13,14 @@ def predict_release_risk(release_properties, path_to_model = 'trained/model_lr.p
         2, #'Criticality of Streams'   
     ]]
     """
-    with open(path_to_model, 'rb') as f:
+    # Получаем абсолютный путь к файлу, учитывая местоположение скрипта
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(current_dir, path_to_model)
+
+    # Загружаем модель
+    with open(model_path, 'rb') as f:
         m = pickle.load(f)
         
-    return m.predict(release_properties)
+    # Получаем результат предсказания
+    result = m.predict(release_properties)
+    return result.tolist()  # Преобразуем numpy array в обычный список
